77e7580ea2028fa2f1288ef164931ff4d34c954d
Sukprasan Rattanapani <74826344+SukprasarnRatt@users.noreply.github.com>
2026-04-06 01:28:40 -0500

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int left = 0;
+        int numRow = matrix.size(); // number of row
+        int numCol = matrix[0].size(); // number of collumn
+        int right = (numRow * numCol) - 1;
+        int rowPos, colPos, mid;
+
+
+        while(left <= right){
+            // Get the middle Pos
+            mid = left + (right - left)/2;
+            rowPos = mid / numCol;
+            colPos = mid % numCol;
+
+            if(matrix[rowPos][colPos] > target){
+                right = mid - 1;
+            }else if(matrix[rowPos][colPos] < target){
+                left = mid + 1;
+            }else if (matrix[rowPos][colPos] == target){
+                return true;
+            }
+
+
+            
+        }
+
+        return false;
+        
+    }
+};
